74588277cf2a814af15afb9094ee3e462aa99b5e
Jenny Lee <jennytoralee@gmail.com>
2016-06-22 17:58:45 -0700

    added sharing options

--- app/src/main/java/com/example/jennytlee/nytimessearcher/ArticleActivity.java
+++ app/src/main/java/com/example/jennytlee/nytimessearcher/ArticleActivity.java
@@ -1,8 +1,13 @@
 package com.example.jennytlee.nytimessearcher;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -15,6 +20,7 @@ import butterknife.ButterKnife;
 public class ArticleActivity extends AppCompatActivity {
 
     @BindView(R.id.wvArticle) WebView wvArticle;
+    private ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +47,20 @@ public class ArticleActivity extends AppCompatActivity {
             wvArticle.loadUrl(article.getWebUrl());
         }
 
-
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_article, menu);
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+
+        shareIntent.putExtra(Intent.EXTRA_TEXT, wvArticle.getUrl());
+
+        shareActionProvider.setShareIntent(shareIntent);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
